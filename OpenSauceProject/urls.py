215e771dedd0ea681cdd0b8603cb48816f4ba887
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-02-20 10:37:04 +0100

    redoing the project with proper naming and an app

--- OpenSauceProject/urls.py
+++ OpenSauceProject/urls.py
@@ -1,4 +1,4 @@
-"""OpenSauce URL Configuration
+"""OpenSauceProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
