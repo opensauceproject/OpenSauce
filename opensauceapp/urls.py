93d23caf6bb9256bee17f4d29c1cd02268cc47d2
malik <malik.fleury@he-arc.ch>
2019-02-28 18:52:04 +0100

    Rename admin as reports and add login and logout urls

--- opensauceapp/urls.py
+++ opensauceapp/urls.py
@@ -3,10 +3,15 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 
 from . import views
+from django.contrib.auth import views as reg_views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path(r'lobby/<lobby_name>/', views.lobby, name='lobby'),
     path(r'lobby/', RedirectView.as_view(url='/'), name='lobby'),
-    path(r'admin/', views.admin, name='admin'),
+    path(r'reports/', views.reports, name='reports'),
+
+    # Auth. System
+    path('login/', reg_views.LoginView.as_view(), name='login'),
+    path('logout/', reg_views.LogoutView.as_view(), name='logout'),
 ]
--- opensauceapp/views.py
+++ opensauceapp/views.py
@@ -14,6 +14,6 @@ def index(request):
 def lobby(request, lobby_name):
     return render(request, 'opensauceapp/lobby.html', {'lobby_name': lobby_name, 'lobby_name_json' : json.dumps(lobby_name)})
 
-def admin(request):
+def reports(request):
     context = {}
-    return render(request, 'opensauceapp/admin.html', context)
+    return render(request, 'opensauceapp/reports.html', context)
