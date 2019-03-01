a064250f39332b6fdd18bf234e43b53ac5d93c99
malik <malik.fleury@he-arc.ch>
2019-03-01 17:15:58 +0100

    Disable cache for reports page

--- opensauceapp/views.py
+++ opensauceapp/views.py
@@ -4,6 +4,7 @@ from django.views import generic, View
 from django.urls import reverse_lazy
 from django.utils.safestring import mark_safe
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 import json
 
 from .models import Sauce, Category
@@ -15,6 +16,7 @@ def index(request):
 def lobby(request, lobby_name):
     return render(request, 'opensauceapp/lobby.html', {'lobby_name': lobby_name, 'lobby_name_json' : json.dumps(lobby_name)})
 
+@never_cache
 @login_required
 def reports(request):
     context = {}
