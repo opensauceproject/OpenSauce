ab8eeb0a2c5e65287a01a378c2838a434a2ce126
damianpetroff <damian.petroff@he-arc.ch>
2019-03-06 15:48:18 +0100

    populated fixture sauce.json

--- opensauceapp/models.py
+++ opensauceapp/models.py
@@ -18,7 +18,6 @@ class BaseModel(models.Model):
 class Category(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=200)
-    description=models.TextField()
     def __str__(self):
         return self.name
 
