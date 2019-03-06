38021c6601cd46b3c4bf3608556f4bfaf42840d9
damianpetroff <damian.petroff@he-arc.ch>
2019-03-06 16:11:54 +0100

    create lobby button is activated only when the lobby name field contains something

--- opensauceapp/static/ressources/js/script.js
+++ opensauceapp/static/ressources/js/script.js
@@ -1,8 +1,12 @@
-$(function() {
-    $('#spinner').spinner({
-        value: 20,
-        min: 1,
-        max: 20,
-        step: 1
-    });
+$(document).ready(function(){
+    $('#create_lobby_button').attr('disabled',true);
+    $('#input_lobby_name').keyup(function(){
+        if($(this).val().length !=0){
+            $('#create_lobby_button').attr('disabled', false);
+        }
+        else
+        {
+            $('#create_lobby_button').attr('disabled', true);
+        }
+    })
 });
