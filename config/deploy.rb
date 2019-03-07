3a938dbebfacc7c03cdecee83eb1e9bcb5ab5c49
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-03-07 16:02:56 +0100

    config prod capitruc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "OpenSauce"
+set :repo_url, "git@github.com:HE-Arc/OpenSauce.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
