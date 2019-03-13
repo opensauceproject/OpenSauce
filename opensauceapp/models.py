9c6c40562f256dba1c97d92439ea6534eb25f114
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-03-13 17:02:44 +0100

    begin add reports models + new migration

--- opensauceapp/models.py
+++ opensauceapp/models.py
@@ -14,4 +14,20 @@ class Sauce(models.Model):
     difficulty=models.IntegerField()
     media_type=models.IntegerField()
 
+class ReportCategory(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=300)
+
+class Report(models.Model):
+    id=models.AutoField(primary_key=True)
+    sauce=models.ForeignKey(Sauce, on_delete=models.CASCADE)
+    additional_informations=models.CharField(max_length=500)
+
+class ReportReportCategory(models.Model):
+    id=models.AutoField(primary_key=True)
+    report=models.ForeignKey(Report, on_delete=models.CASCADE)
+    report_category=models.ForeignKey(ReportCategory, on_delete=models.CASCADE)
+
+
 
