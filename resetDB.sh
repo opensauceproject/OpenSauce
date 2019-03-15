e9ba1e23370584ba1116ca6d602e92d6667fa675
Raphael Margueron <raphael.margueron@gmail.com>
2019-03-15 20:04:11 +0100

    add report categories and reset db scripts

---
+++ resetDB.sh
@@ -0,0 +1,5 @@
+rm db.sqlite3
+python manage.py migrate
+python manage.py loaddata opensauceapp/fixtures/SauceCategory.json
+python manage.py loaddata opensauceapp/fixtures/ReportCategory.json
+pause
