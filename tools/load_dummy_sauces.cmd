daf6f7ec436a9372887676b130cc941801cc79cf
Raphael Margueron <raphael.margueron@gmail.com>
2019-03-24 18:14:52 +0100

    moving tools files

---
+++ tools/load_dummy_sauces.cmd
@@ -0,0 +1,2 @@
+cd ..
+python manage.py loaddata opensauceapp\fixtures\Sauce.json
--- tools/reset_DB.cmd
+++ tools/reset_DB.cmd
@@ -1,7 +1,7 @@
+cd ..
 del db.sqlite3
 python manage.py migrate
 python manage.py loaddata opensauceapp\fixtures\SauceCategory.json
 python manage.py loaddata opensauceapp\fixtures\ReportCategory.json
 python manage.py loaddata opensauceapp\fixtures\Sauce.json
 python manage.py createsuperuser --username admin --email admin@opensauce.ch
-pause
