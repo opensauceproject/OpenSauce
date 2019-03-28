5b8d0bd84f61e5eedfda70e22abbf476fdb71f7c
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-03-28 15:07:31 +0100

    12345

--- opensauceproject/urls.py
+++ opensauceproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('opensauceapp.urls')),
