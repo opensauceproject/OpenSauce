da7456035b40d23f4c451681d2a29ee2d0208dbc
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-03-28 14:46:06 +0100

    123

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,9 +28,7 @@ namespace :python do
         on roles([:app, :web]) do |h|
 	    execute "rm -rf #{venv_path}"
 	    execute "python -m venv #{venv_path}"
-        execute "source #{venv_path}/bin/activate"
-        execute "python --version"
-	    execute "python -m pip install -r #{release_path}/requirements.txt"
+	    execute "source #{venv_path}/bin/activate; python -m pip install -r #{release_path}/requirements.txt"
         end
     end
 end
